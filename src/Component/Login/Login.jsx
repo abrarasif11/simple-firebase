cf336735cab7647eed171fce9b4c9c1c75b9e350
abrarasif11 <faasif929@gmail.com>
2025-08-18 19:14:29 +0600

    log-out-added

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import React, { useState } from 'react'
 import auth from '../../firebase/firebase.init'
 
@@ -18,7 +18,13 @@ const Login = () => {
 
   }
   const handleLogOut = () =>{
-    
+    signOut(auth)
+    .then(() => {
+      setUser(null);
+    })
+    .catch(error => 
+      console.log(error)
+    )
   }
   return (
     <div>
