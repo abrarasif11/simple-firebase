f56e272289a7dfe296fcc68e499164f970449ce1
abrarasif11 <faasif929@gmail.com>
2025-08-18 12:26:17 +0600

    google-signup-popup

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,15 +1,24 @@
-import { GoogleAuthProvider } from 'firebase/auth/web-extension'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import React from 'react'
+import auth from '../../firebase/firebase.init'
 
 const Login = () => {
   const provider = new GoogleAuthProvider()
   const handleGoogleSignIn = () =>{
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result)
+    })
+    .catch(error => {
+      console.log('Error', error)
+
+    })
 
   }
   return (
     <div>
       Login page
-      <button onClick={}>Login With Google..</button>
+      <button onClick={handleGoogleSignIn}>Login With Google..</button>
     </div>
   )
 }
